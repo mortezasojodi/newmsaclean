647b56e5f60b18e8490df96c6035e44a50682fec
Morteza Sojodi <mailto:mortezasojodi70@gmail.com>
2023-05-15 00:53:58 +0330

    .

--- app/src/main/java/com/example/newcleanmsa/MainActivity.kt
+++ app/src/main/java/com/example/newcleanmsa/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.newcleanmsa
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.newcleanmsa.Product.ProductActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val intent=Intent(this,ProductActivity::class.java)
+        startActivity(intent)
     }
 }
